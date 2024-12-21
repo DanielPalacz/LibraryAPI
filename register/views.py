bea7a70551825c0beced2294bf93d975a875675f
Daniel <daniel.palacz@gmail.com>
2024-12-21 20:50:29 +0100

    updates RegisterView

--- register/views.py
+++ register/views.py
@@ -4,19 +4,33 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.shortcuts import render
 from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import OpenApiResponse
 from rest_framework import status  # type: ignore
 from rest_framework.exceptions import ValidationError  # type: ignore
+from rest_framework.permissions import AllowAny  # type: ignore
 from rest_framework.response import Response  # type: ignore
 from rest_framework.views import APIView  # type: ignore
 
+from register.serializers import UserSerializer
+
 
 class RegisterView(APIView):  # type: ignore
+
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]  # This turns off authentication
+
     @extend_schema(
         summary="User Registration",
         description="Endpoint to register a new user.",
         tags=["User Management"],
-        request={"application/json": {"username": "string", "email": "string", "password": "string"}},
-        responses={201: {"message": "User registered successfully"}},
+        responses={
+            201: OpenApiResponse(
+                description="User registered successfully",
+            ),
+            400: OpenApiResponse(
+                description="Bad Request - Username taken or invalid password",
+            ),
+        },
     )
     def post(self, request):  # type: ignore
         username = request.data.get("username")
